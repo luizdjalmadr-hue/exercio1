1b3078678086644b8558d2146ed70cd96f8be61a
Djalma Luiz de Melo Duarte da Silva <13526111215@ulife.com.br>
2026-08-20 16:05:54 -0300

    primeiro commit

---
+++ minhas atividades.py/abc.py
@@ -0,0 +1,27 @@
+#respostas aula 1 
+#exercicio 1
+#manter o site da prefeitura no ar (operação)
+#migrar o site da prefeitura para uma nova plataforma (projeto)
+#atender os chamados de suporte da secretaria (operação)
+#implantar um sistema de chamados para a secretaria (projeto)
+#emitir mensalmente a folha de pagamento (operação)
+#trocar o sistema que emiti a folha de pagamento (projeto)
+
+#exercicio 2 
+#A premissa de maior impacto e a que os dados de endereço e cadstro dos alunos ja estão sendo coletados em formato utilizave. se for falsa ou seja se os dados informados aina estiverem so em papel ou fichas sera preciso realizar o tablho de digitalizar os 3200 registros que não estava previsto no inicio que aumentaria o prazo e custo do projeto gerando mais despesas extras que estavam planejadas.
+
+#exercicio 3
+# 90% dos servidores conegue realizar uma tarefa comun ate 3 minutos apos 1 horado treinamento. quem define: os proprios servidores.
+#o realatorio completo da prestação das contas sera gerado em ate 5 minutos. quem define: a secretaria de Educação.
+#o sistema ficara disponivel somente durante o horario comercial com no maximo 1 interrupção. quem define: a equipe de Ti.
+
+#exercicio 4
+# analisar e apenas construir o que e necessario para os clientes. 1 causa
+# 2 causa: busca conversa com o usuario para enterde o que ele precisa.
+# 3 causa:busca pesquisa sobre o que faz para não da um prazo maior ou curto.
+
+#exercicio 5
+#acrescentar mais pessoas ao projeto atrasado pode atrasar ainda mais.por que pessoas novas precisam aprender tudo que ja foi feito e o que ainda vai ser feito quem ja estar no projeto tera que para seu trabalho para ensinar quem não sabe de nada o que fara a produção ser mais demorada.
+
+#exercicio 6
+#por que nesse caso a equipe pediu mais tempo porem não que dizer que esse projeto vai ser cumprido com esse tempo adicional por que pode aver mudanças ao longo do projeto e tambem a preguiça de alguns trabalhadores.
